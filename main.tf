addbf20a2e643cc6895665b1f4523b3c997153b3
Bay <baycele@gmail.com>
2023-06-26 17:40:03 +0000

    Updated project ID

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "pro-plasma-391016" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
